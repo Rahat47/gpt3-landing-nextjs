210c0417969369e759cc7ed1ace5ff7e4253e0cf
Rahat47 <djrayhan8@gmail.com>
2021-11-15 13:42:59 +0600

    working on header

--- containers/header/Header.jsx
+++ containers/header/Header.jsx
@@ -1,9 +1,46 @@
 import styles from './header.module.css';
+import ai from '../../assets/images/ai.png';
+import people from '../../assets/images/people.png';
+import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className={`${styles.header} section__padding`} id='home'>
+            <div className={styles.header_content}>
+                <h1 className='gradient__text'>
+                    Let’s Build Something amazing with GPT-3 OpenAI
+                </h1>
+                <p>
+                    Yet bed any for travelling assistance indulgence unpleasing.
+                    Not thoughts all exercise blessing. Indulgence way
+                    everything joy alteration boisterous the attachment. Party
+                    we years to order allow asked of.
+                </p>
+                <div className={styles.header_content_input}>
+                    <input
+                        type='email'
+                        name='email'
+                        id='email'
+                        placeholder='Enter your email...'
+                    />
+                    <button type='button'>Get Started</button>
+                </div>
+
+                <div className={styles.header_content_people}>
+                    <Image
+                        src={people}
+                        alt='people that use our app'
+                        width={182}
+                        height={38}
+                    />
+                    <p>
+                        1,600 people requested access a visit in last 24 hours
+                    </p>
+                </div>
+            </div>
+            <div className={styles.header_img}>
+                <Image src={ai} alt='ai' />
+            </div>
         </div>
     );
 };
