1d7309f1c2c6a14a17876881858673ba1d11ac6a
Rahat47 <djrayhan8@gmail.com>
2021-11-15 19:22:36 +0600

    possibility section done

--- containers/features/Features.jsx
+++ containers/features/Features.jsx
@@ -1,9 +1,27 @@
+import { Feature } from '../../components';
+import { featuresData } from './data';
 import styles from './features.module.css';
 
 const Features = () => {
     return (
-        <div>
-            <h1>Features</h1>
+        <div className={`${styles.features} section__padding`} id='features'>
+            <div className={styles.features_heading}>
+                <h1 className='gradient__text'>
+                    The Future is Now and You Just Need To Realize It. Step into
+                    Future Today & Make it Happen.
+                </h1>
+                <p>Request Early Access to Get Started</p>
+            </div>
+            <div className={styles.features_container}>
+                {featuresData.map(feature => (
+                    <Feature
+                        key={feature.id}
+                        text={feature.text}
+                        title={feature.title}
+                        horz={feature.horz}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
--- containers/possibility/Possibility.jsx
+++ containers/possibility/Possibility.jsx
@@ -1,9 +1,30 @@
 import styles from './possibility.module.css';
+import Image from 'next/image';
+import possibilityImg from '../../assets/images/possibility.png';
 
 const Possibility = () => {
     return (
-        <div>
-            <h1>Possibility</h1>
+        <div
+            className={`${styles.possibility} section__padding`}
+            id='possibility'
+        >
+            <div className={styles.possibility_img}>
+                <Image src={possibilityImg} alt='woman with VR headset' />
+            </div>
+
+            <div className={styles.possibility_content}>
+                <h4>Request Early Access to Get Started</h4>
+                <h1 className='gradient__text'>
+                    The possibilities are beyond your imagination
+                </h1>
+                <p>
+                    Yet bed any for travelling assistance indulgence unpleasing.
+                    Not thoughts all exercise blessing. Indulgence way
+                    everything joy alteration boisterous the attachment. Party
+                    we years to order allow asked of.
+                </p>
+                <h4>Request Early Access to Get Started</h4>
+            </div>
         </div>
     );
 };
