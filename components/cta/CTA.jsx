c9cad8578bf9dff522041383fb13535775c49b76
Rahat47 <djrayhan8@gmail.com>
2021-11-15 19:35:47 +0600

    cta section done

--- components/cta/CTA.jsx
+++ components/cta/CTA.jsx
@@ -2,8 +2,17 @@ import styles from './cta.module.css';
 
 const CTA = () => {
     return (
-        <div>
-            <h1>CTA</h1>
+        <div className={styles.cta}>
+            <div className={styles.cta_content}>
+                <p>Request Early Access to Get Started</p>
+                <h3>
+                    Register today & start exploring the endless possiblities.
+                </h3>
+            </div>
+
+            <div className={styles.cta_button}>
+                <button type='button'>Get Started</button>
+            </div>
         </div>
     );
 };
